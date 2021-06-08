875f3140ba6886f2b948e4c85590331ae774cb6d
djdenney <daniel@denneythe.dev>
2021-06-07 23:02:51 -0700

    added data validation for subscriptions and messages for POST requests

--- src/messages/messages.controller.js
+++ src/messages/messages.controller.js
@@ -8,8 +8,65 @@ async function create(req, res, next) {
     })
 }
 
+function hasData(req, res, next) {
+    if (req.body.data) {
+        return next()
+    }
+    next({
+        status: 400,
+        message: `request body must have data property`
+    })
+}
+
+function hasName(req, res, next) {
+    if (req.body.data.name) {
+        return next()
+    }
+    next({
+        status: 400,
+        message: `data must have name property`
+    })
+}
+
+function hasEmail(req, res, next) {
+    if (req.body.data.email) {
+        return next()
+    }
+    next({
+        status: 400,
+        message: `data must have email property`
+    })
+}
+
+function hasMessage(req, res, next) {
+    if (req.body.data.message) {
+        return next()
+    }
+    next({
+        status: 400,
+        message: `data must have message property`
+    })
+}
+
+function emailIsValid(req, res, next) {
+    const email = req.body.data.email
+    const format = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    if (email.match(format)) {
+        return next()
+    } 
+    next({
+        status: 400,
+        message: `${email} is not a valid email address`
+    })
+}
+
 module.exports = {
     create: [
+        hasData,
+        hasName,
+        hasEmail,
+        hasMessage,
+        emailIsValid,
         asyncErrorBoundary(create),
     ],
 }
--- src/subscriptions/subscriptions.controller.js
+++ src/subscriptions/subscriptions.controller.js
@@ -8,8 +8,43 @@ async function create(req, res, next) {
     })
 }
 
+function hasData(req, res, next) {
+    if (req.body.data) {
+        return next()
+    }
+    next({
+        status: 400,
+        message: `request body must have data property`
+    })
+}
+
+function hasEmail(req, res, next) {
+    if (req.body.data.email) {
+        return next()
+    }
+    next({
+        status: 400,
+        message: `data must have email property`
+    })
+}
+
+function emailIsValid(req, res, next) {
+    const email = req.body.data.email
+    const format = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    if (email.match(format)) {
+        return next()
+    } 
+    next({
+        status: 400,
+        message: `${email} is not a valid email address`
+    })
+}
+
 module.exports = {
     create: [
+        hasData,
+        hasEmail,
+        emailIsValid,
         asyncErrorBoundary(create),
     ],
 }
