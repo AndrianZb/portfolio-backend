fe18ebce99bc8d7c7257853151144f4e9d912e04
djdenney <daniel@denneythe.dev>
2021-07-05 09:28:44 -0700

    updated route info

--- src/app.js
+++ src/app.js
@@ -11,8 +11,15 @@ const corsOptions = {
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
-const messagesRouter = require("./messages/messages.router");
-const subscriptionsRouter = require("./subscriptions/subscriptions.router");
+const usersRouter = require("./users/users.router");
+const pagesRouter = require("./pages/pages.router");
+const articlesRouter = require("./articles/articles.router");
+const formsRouter = require("./forms/forms.router");
+const objectsRouter = require("./objects/objects.router");
+const fieldsRouter = require("./fields/fields.router");
+const imagesRouter = require("./images/images.router");
+const linksRouter = require("./links/links.router");
+const submissionsRouter = require("./submissions/submissions.router");
 
 const app = express();
 
@@ -20,8 +27,15 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.options("*", cors());
-app.use("/messages", messagesRouter);
-app.use("/subscriptions", subscriptionsRouter);
+app.use("/users", usersRouter);
+app.use("/pages", pagesRouter);
+app.use("/articles", articlesRouter);
+app.use("/forms", formsRouter);
+app.use("/objects", objectsRouter);
+app.use("/fields", fieldsRouter);
+app.use("/images", imagesRouter);
+app.use("/links", linksRouter);
+app.use("/submissions", submissionsRouter);
 
 app.use(notFound);
 app.use(errorHandler);
